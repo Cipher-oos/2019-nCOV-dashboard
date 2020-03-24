189e09b1ab55b457a5fdeda2fe6eeb3b827ca7bd
Cipher-oos <62554660+Cipher-oos@users.noreply.github.com>
2020-03-24 16:40:56 +0200

    Add files via upload

---
+++ sections/content_overview/map.R
@@ -0,0 +1,113 @@
+library("htmltools")
+
+addLabel <- function(data) {
+  data$label <- paste0(
+    '<b>', ifelse(is.na(data$`Province/State`), data$`Country/Region`, data$`Province/State`), '</b><br>
+    <table style="width:120px;">
+    <tr><td>Confirmed:</td><td align="right">', data$confirmed, '</td></tr>
+    <tr><td>Deceased:</td><td align="right">', data$deceased, '</td></tr>
+    <tr><td>Recovered:</td><td align="right">', data$recovered, '</td></tr>
+    <tr><td>Active:</td><td align="right">', data$active, '</td></tr>
+    </table>'
+  )
+  data$label <- lapply(data$label, HTML)
+
+  return(data)
+}
+
+map <- leaflet(addLabel(data_latest)) %>%
+  setMaxBounds(-180, -90, 180, 90) %>%
+  setView(0, 0, zoom = 2) %>%
+  addTiles() %>%
+  addProviderTiles(providers$CartoDB.Positron, group = "Light") %>%
+  addProviderTiles(providers$HERE.satelliteDay, group = "Satellite") %>%
+  addLayersControl(
+    baseGroups    = c("Light", "Satellite"),
+    overlayGroups = c("Confirmed", "Confirmed (per capita)", "Recovered", "Deceased", "Active", "Active (per capita)"),
+    options       = layersControlOptions(collapsed = FALSE)
+  ) %>%
+  hideGroup("Confirmed (per capita)") %>%
+  hideGroup("Recovered") %>%
+  hideGroup("Deceased") %>%
+  hideGroup("Active") %>%
+  hideGroup("Active (per capita)")
+
+observe({
+  req(input$timeSlider, input$overview_map_zoom)
+  zoomLevel               <- input$overview_map_zoom
+  data                    <- data_atDate(input$timeSlider) %>% addLabel()
+  data$confirmedPerCapita <- data$confirmed / data$population * 100000
+  data$activePerCapita    <- data$active / data$population * 100000
+
+  leafletProxy("overview_map", data = data) %>%
+    clearMarkers() %>%
+    addCircleMarkers(
+      lng          = ~Long,
+      lat          = ~Lat,
+      radius       = ~log(confirmed^(zoomLevel / 2)),
+      stroke       = FALSE,
+      fillOpacity  = 0.5,
+      label        = ~label,
+      labelOptions = labelOptions(textsize = 15),
+      group        = "Confirmed"
+    ) %>%
+    addCircleMarkers(
+      lng          = ~Long,
+      lat          = ~Lat,
+      radius       = ~log(confirmedPerCapita^(zoomLevel)),
+      stroke       = FALSE,
+      color        = "#00b3ff",
+      fillOpacity  = 0.5,
+      label        = ~label,
+      labelOptions = labelOptions(textsize = 15),
+      group        = "Confirmed (per capita)"
+    ) %>%
+    addCircleMarkers(
+      lng          = ~Long,
+      lat          = ~Lat,
+      radius       = ~log(recovered^(zoomLevel)),
+      stroke       = FALSE,
+      color        = "#005900",
+      fillOpacity  = 0.5,
+      label        = ~label,
+      labelOptions = labelOptions(textsize = 15),
+      group        = "Recovered"
+    ) %>%
+    addCircleMarkers(
+      lng          = ~Long,
+      lat          = ~Lat,
+      radius       = ~log(deceased^(zoomLevel)),
+      stroke       = FALSE,
+      color        = "#E7590B",
+      fillOpacity  = 0.5,
+      label        = ~label,
+      labelOptions = labelOptions(textsize = 15),
+      group        = "Deceased"
+    ) %>%
+    addCircleMarkers(
+      lng          = ~Long,
+      lat          = ~Lat,
+      radius       = ~log(active^(zoomLevel / 2)),
+      stroke       = FALSE,
+      color        = "#f49e19",
+      fillOpacity  = 0.5,
+      label        = ~label,
+      labelOptions = labelOptions(textsize = 15),
+      group        = "Active"
+    ) %>%
+    addCircleMarkers(
+      lng          = ~Long,
+      lat          = ~Lat,
+      radius       = ~log(activePerCapita^(zoomLevel)),
+      stroke       = FALSE,
+      color        = "#f4d519",
+      fillOpacity  = 0.5,
+      label        = ~label,
+      labelOptions = labelOptions(textsize = 15),
+      group        = "Active (per capita)"
+    )
+})
+
+output$overview_map <- renderLeaflet(map)
+
+
---
+++ ui.R
@@ -0,0 +1,29 @@
+source("sections/ui_overview.R", local = TRUE)
+source("sections/ui_plots.R", local = TRUE)
+source("sections/ui_about.R", local = TRUE)
+source("sections/ui_fullTable.R", local = TRUE)
+
+ui <- tagList(
+  tags$head(
+    tags$link(rel = "shortcut icon", type = "image/png", href = "logo.png")
+  ),
+  tags$style(type = "text/css", ".container-fluid {padding-left: 0px; padding-right: 0px !important;}"),
+  tags$style(type = "text/css", ".navbar {margin-bottom: 0px;}"),
+  tags$style(type = "text/css", ".content {padding: 0px;}"),
+  tags$style(type = "text/css", ".row {margin-left: 0px; margin-right: 0px;}"),
+  tags$style(HTML(".col-sm-12 { padding: 5px; margin-bottom: -15px; }")),
+  tags$style(HTML(".col-sm-6 { padding: 5px; margin-bottom: -15px; }")),
+  navbarPage(
+    title       = "COVID-19 Global Monitor",
+    collapsible = TRUE,
+    fluid       = TRUE,
+    tabPanel("Overview", page_overview, value = "page-overview"),
+    tabPanel("Table", page_fullTable, value = "page-fullTable"),
+    tabPanel("Plots", page_plots, value = "page-plots"),
+    tabPanel("About", page_about, value = "page-about"),
+    tags$script(HTML("var header = $('.navbar > .container-fluid');
+    header.append('<div style=\"float:right\"><a target=\"_blank\" href=\"https://github.com/chschoenenberger/covid19_dashboard\"><img src=\"logo.png\" alt=\"alt\" style=\"float:right;width:33px;padding-top:10px;margin-top:-50px;margin-right:10px\"> </a></div>');
+    console.log(header)")
+    )
+  )
+)
